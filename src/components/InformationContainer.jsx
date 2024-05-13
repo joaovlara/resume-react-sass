71bb43ac7b930a9413fdfe90890685aada1f6c8f
Joao Lara <joaovdelara@gmail.com>
2024-05-13 10:30:17 -0300

    add typing animations and appear

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -2,26 +2,24 @@ import "../styles/components/informationcontainer.sass";
 
 import { AiFillPhone, AiOutlineMail, AiFillEnvironment } from "react-icons/ai";
 
+const informations = [
+  { id: "phone", icon: <AiFillPhone />, data: "(41)9 9820-5576" },
+  { id: "email", icon: <AiOutlineMail />, data: "joaovdelara@gmail.com" },
+  { id: "pin", icon: <AiFillEnvironment />, data: "Curitiba / PR" },
+];
+
 const InformationContainer = () => {
   return (
     <section id="information">
-      <div className="info-card">
-        <AiFillPhone id="phone-icon" />
-        <div>
-          <p>(41)9 9820-5576</p>
-        </div>
-      </div>
-      <div className="info-card">
-        <AiOutlineMail id="email-icon" />
-        <div>
-          <p>joaovdelara@gmail.com</p>
-        </div>
-      </div>
-      <div className="info-card">
-        <AiFillEnvironment id="pin-icon" />
-        <div>
-          <p>Curitiba / PR</p>
-        </div>
+      <div className="informations-grid">
+        {informations.map((info) => (
+          <div className="info-card" >
+            <div className="info-typing" id={info.id}>
+              {info.icon} {info.data}
+            </div>
+            <br />
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,13 +5,17 @@ import Avatar from "../img/jwao.png"
 
 import "../styles/components/sidebar.sass";
 
-
 const Sidebar = () => {
     return (
         <aside id="sidebar">
             <img src={Avatar} alt="João Lara" />
-            <h2>João Vitor de Lara</h2>
-            <p className="title">Desenvolvedor Front-End</p>
+
+            <div className="name">
+                <h2>João Vitor de Lara</h2>
+            </div>
+            <div className="title">
+                <p className="title-text">Desenvolvedor Front-End</p>
+            </div>
             <SocialNetworkContainer />
             <InformationContainer />
             <a href="https://drive.google.com/file/d/19mX9BNREcsYxh6kGjZADtBTAyu0n5fY0/view?usp=sharing" className="btn">Dowload CV</a>
